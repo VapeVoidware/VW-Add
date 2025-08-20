078761191d17975871d1a2ade67218ba1c8943bd
Erchobg <67239055+Erchobg@users.noreply.github.com>
2025-08-20 19:06:53 +0300

    Update nightsintheforest.lua

--- nightsintheforest.lua
+++ nightsintheforest.lua
@@ -70,7 +70,7 @@ if game.PlaceId == 79546208627805 then
     pcall(function()
         game:GetService("StarterGui"):SetCore("SendNotification", {
             Title = "Voidware | 99 Nights In The Forest",
-            Text = "Go In Game for Voidware to load :D",
+            Text = "Go In Game for Voidware to load :D [You are in lobby currently]",
             Duration = 10
         })
     end)
