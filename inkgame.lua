e7973e95aabaf3b022514835d0bd3addfe63b26a
Erchobg <67239055+Erchobg@users.noreply.github.com>
2025-09-03 12:25:37 +0300

    Update inkgame.lua

--- inkgame.lua
+++ inkgame.lua
@@ -163,7 +163,7 @@ if IS_DOWN and not shared.BYPASS_VW_PROTECTION then
     })
 
 else
-    local commit = shared.CustomCommit and tostring(shared.CustomCommit) or shared.StagingMode and "staging" or "05e1141b8fdeec9dd262dde9f071d6ffdaccd5b1"
+    local commit = shared.CustomCommit and tostring(shared.CustomCommit) or shared.StagingMode and "staging" or "471249fc5c6e9f21dd590094dd5436d596b5b757"
 
     local verified_executors = {"jjs", "valex", "hydrogen", "delta", "solara", "krnl"}
     local suc, current_executor = pcall(function()
