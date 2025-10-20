aaf67cab3fb930d71d17678545fc3613e6ede4a8
Erchobg <67239055+Erchobg@users.noreply.github.com>
2025-10-20 16:40:43 +0300

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -22,22 +22,22 @@ local meta = {
     [2619619496] = {
         title = "Bedwars",
         dev = "vwdev/vwrw.lua",
-        script = "https://raw.githubusercontent.com/VapeVoidware/VWRewrite/main/NewMainScript.lua"
+        script = "https://raw.githubusercontent.com/VapeVoidware/VWRewrite/"..(shared.CustomCommit and tostring(shared.CustomCommit) or "main").."/NewMainScript.lua"
     },
     [7008097940] = {
        title = "Ink Game",
        dev = "vwdev/inkgame.lua",
-       script = "https://raw.githubusercontent.com/VapeVoidware/VW-Add/main/inkgame.lua"
+       script = "https://raw.githubusercontent.com/VapeVoidware/VW-Add/"..(shared.CustomCommit and tostring(shared.CustomCommit) or "main").."/inkgame.lua",
     },
     [6331902150] = {
         title = "Forsaken",
         dev = "vwdev/forsaken.lua",
-        script = "https://raw.githubusercontent.com/VapeVoidware/VW-Add/main/forsaken.lua"
+        script = "https://raw.githubusercontent.com/VapeVoidware/VW-Add/"..(shared.CustomCommit and tostring(shared.CustomCommit) or "main").."/forsaken.lua"
     },
     [7326934954] = {
         title = "99 Nights In The Forest",
         dev = "vwdev/nightsintheforest.lua",
-        script = "https://raw.githubusercontent.com/VapeVoidware/VW-Add/main/nightsintheforest.lua"
+        script = "https://raw.githubusercontent.com/VapeVoidware/VW-Add/"..(shared.CustomCommit and tostring(shared.CustomCommit) or "main").."/nightsintheforest.lua"
     }
 }
 local data = meta[game.GameId]
